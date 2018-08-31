54ca7ec6a952f1d80883e070099c70ac1ee32a2f
root <root@cpe-172-72-33-11.carolina.res.rr.com>
2018-08-31 16:42:46 +0800

    fixed stuff

--- utils/function.go
+++ utils/function.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"github.com/funlake/gopkg/utils/log"
 	"unsafe"
+	"bytes"
+	"encoding/json"
 )
 
 func RoutineRecover(msg string){
@@ -24,4 +26,14 @@ func StrToByte(s string) []byte{
 
 func ByteToStr(s []byte) string{
 	return *(*string)(unsafe.Pointer(&s))
+}
+
+//设置html过滤为false,否则默认的json marshal会过滤html字符,
+//导致返回数据不是raw string
+func JSONMarshal(t interface{}) ([]byte, error) {
+	buffer := &bytes.Buffer{}
+	encoder := json.NewEncoder(buffer)
+	encoder.SetEscapeHTML(false)
+	err := encoder.Encode(t)
+	return buffer.Bytes(), err
 }
