c33b00facf57c22af9758e1e1ab9c49d60127506
root <root@api-auth.etcchebao.com>
2019-03-06 16:23:05 +0800

    avoid unexcept redis nil return

--- cache/timercache_redis.go
+++ cache/timercache_redis.go
@@ -13,10 +13,11 @@ type TimerCacheRedis struct{
 	store *KvStoreRedis
 	local map[string] string
 	ticker *timer.Ticker
+	emptyCount map[string] int
 }
 
 func NewTimerCacheRedis() *TimerCacheRedis{
-	return &TimerCacheRedis{local:make(map[string] string),ticker:timer.NewTicker()}
+	return &TimerCacheRedis{local:make(map[string] string),ticker:timer.NewTicker(),emptyCount:make(map[string] int)}
 }
 func (tc *TimerCacheRedis) Flush(){
 	tc.mu.Lock()
@@ -53,9 +54,17 @@ func (tc *TimerCacheRedis) Get(hk string,k string,wheel int) (string,error){
 				//假如redis服务器挂了,得保留之前的本地缓存值
 				if err != nil {
 					if strings.Contains(err.Error(),"nil returned"){
-						log.Error("Empty value deteced(%d s) : %s,remove ticker run: error:%s",wheel,localCacheKey,err.Error())
-						//如果值返回为空，则停掉定时更新器
-						tc.ticker.Stop(wheel,localCacheKey)
+						if _,ok:=tc.emptyCount[localCacheKey];ok{
+							tc.emptyCount[localCacheKey]++
+						} else {
+							tc.emptyCount[localCacheKey] = 1
+						}
+						//如果值返回为空，且超过3次，则停掉定时更新器(测试环境发现偶尔会有异常情况下的空值返回)
+						if tc.emptyCount[localCacheKey] > 3 {
+							log.Error("Empty value deteced(%d s) at least 3 times : %s,remove ticker run: error:%s",wheel,localCacheKey,err.Error())
+							tc.ticker.Stop(wheel, localCacheKey)
+							delete(tc.emptyCount,localCacheKey)
+						}
 						//赋空值,如要情况缓存，可调用/api-cleancache接口
 						tc.local[localCacheKey] = v.(string)
 						//delete(tc.local,localCacheKey)
@@ -65,6 +74,10 @@ func (tc *TimerCacheRedis) Get(hk string,k string,wheel int) (string,error){
 					}
 					//delete(tc.local, localCacheKey)
 				}else{
+					if _,ok:=tc.emptyCount[localCacheKey];ok{
+						//成功清楚空值计数
+						delete(tc.emptyCount,localCacheKey)
+					}
 					//log.Warning("更新数据%s : %s",localCacheKey,v.(string))
 					tc.local[localCacheKey] = v.(string)
 				}
